0260f125ec95c1c09dba3634d0b5b88d321f921f
SotoLR <44563359+SotoLR@users.noreply.github.com>
2019-12-06 10:25:24 +0100

    Created phrase memorization tool. WIP

---
+++ phraser_prototype.py
@@ -0,0 +1,27 @@
+import msvcrt
+
+def main():
+	exit_char = '*'
+	exit_str_len = 5
+
+	while True:
+		c = str(msvcrt.getch())[2] #Get input, no need to hit enter. Doesn't work with special input chars (ctrl+c, for example)
+		#										Process received code. Typing 'a' will cause msvcrt.getch() to output " b'a "
+		#											and we only need 'a'
+		if c == exit_char: 						#Detect first char of exit sequence
+			exit_string = exit_char
+			while True:							#Exit sequence process loop
+				c = str(msvcrt.getch())[2]
+				if c == exit_char:
+					exit_string += c
+				else:
+					print(exit_char, end="")	#Turns any character into a postfix escape sequence for the exit char
+					break
+				if exit_string == exit_char*exit_str_len: #Exit program
+					return
+				if exit_string == exit_char*(exit_str_len-1): #Warn before entering last char
+					print()
+					print(f"Enter {exit_char} once more to exit. Enter anything else to continue.")		
+		print(c, end="", flush=True)			#Print character. Ironically, this single line is the important part of this script
+
+main()
